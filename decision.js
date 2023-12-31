bba1248454ba4fd2683817ba85b016bb3fe56d13
websym <szones333@gmail.com>
2023-12-31 18:29:34 +0545

    - m "first commit"

---
+++ decision.js
@@ -0,0 +1,37 @@
+/*
+let counter=3;
+
+if (counter==4) {
+    console.log('counter is equal to 3');
+}else if(counter>5){
+    console.log('counter is greater than 5');
+}
+else if(counter<5){
+    console.log('counter is less than 5');
+} else
+{
+    console.log('counter is not equal to 3');
+}
+*/
+
+/*
+let hero='salman';
+switch(hero){
+    case 'batman':
+        console.log('intelligense and fighting skills');
+        break;
+    case 'ironman':
+        console.log('more skills and more powerfull');
+        break;
+    case'spiderman':
+        console.log('its really amazing');
+        break;
+    default:
+        console.log('Not a hero');
+        break;
+}
+*/
+//ternary operators
+let a=1, b=2;
+let result=(a==b) ? 'equals': ' enqualus';
+console.log(result);
