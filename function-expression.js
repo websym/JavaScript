8de6b4de97e2adcd04afc88b39ee80d6fe748eef
websym <szones333@gmail.com>
2023-12-31 18:02:49 +0545

    first commit

--- function-expression.js
+++ function-expression.js
@@ -1,4 +1,18 @@
-setTimeout(function()
-{
-    console.log('I waited 2 seconds');
-},2000);
+// setTimeout(function()
+// {
+//     console.log('I waited 2 seconds');
+// },2000);
+
+/*
+let counter = 0;
+function timeout(){
+    setTimeout(function(){
+        console.log('hi',counter++);
+        timeout();
+    },100);
+}
+timeout();
+*/
+(function(){
+    console.log('Immediate Invoked Funtion Expression','IIFE');
+})();
